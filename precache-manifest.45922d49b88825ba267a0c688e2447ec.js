ef53a8c925dd73dd8fbd0696f251e55207617819
Vladimir Bezobrazov <vovan0503@mail.ru>
2020-08-15 16:25:44 +0300

    Updates

---
+++ precache-manifest.45922d49b88825ba267a0c688e2447ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18c71228c78c7a26422c06e72cf70a2",
+    "url": "/Weather_dashboard/index.html"
+  },
+  {
+    "revision": "517a605498276eaea041",
+    "url": "/Weather_dashboard/static/css/main.80327df0.chunk.css"
+  },
+  {
+    "revision": "226e0eeed230f12997b4",
+    "url": "/Weather_dashboard/static/js/2.c4e5d6ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Weather_dashboard/static/js/2.c4e5d6ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "517a605498276eaea041",
+    "url": "/Weather_dashboard/static/js/main.d053df1b.chunk.js"
+  },
+  {
+    "revision": "e0534a933edd11983c97",
+    "url": "/Weather_dashboard/static/js/runtime-main.b5568bbc.js"
+  }
+]);
